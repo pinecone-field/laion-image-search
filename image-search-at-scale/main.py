f1bd4d45366735689ba8d131974dfc79ff47fb11
TSCM-658 <thomas.m@pinecone.io>
2024-07-08 12:42:39 +0100

    Adding text search functionality

--- image-search-at-scale/main.py
+++ image-search-at-scale/main.py
@@ -15,16 +15,20 @@ load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
 IMAGE_PATH = "./static/image.jpeg"
+TEXT_PATH = "./static/text.txt"
 MODEL = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 PROCESSOR = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 CACHED_IMAGE_HASH = None
 CACHED_EMBEDDING = None
+CACHED_TEXT = None
+CACHED_TEXT_EMBEDDING = None 
+
 def get_image_hash(image_path):
     with open(image_path, 'rb') as f:
         return hashlib.md5(f.read()).hexdigest()
-
-def get_embedding():
+    
+def get_image_embedding():
     global CACHED_IMAGE_HASH
     global CACHED_EMBEDDING
     start_time = time.time()
@@ -56,13 +60,38 @@ def get_embedding():
     print(f"Get image embedding execution time: {(end_time - start_time) * 1000} ms")
     return CACHED_EMBEDDING
 
-def pinecone_query(embedding):
+def get_text_embedding():
+    global CACHED_TEXT
+    global CACHED_TEXT_EMBEDDING
+    start_time = time.time()
+    f = open(TEXT_PATH, 'r')
+    text = f.read()
+    f.close()
+
+    #If the text is the same then the embedding is the same
+    if text == CACHED_TEXT: 
+        print("Text has not changed. Using cached text embedding")
+        return CACHED_TEXT_EMBEDDING
+    CACHED_TEXT = text
+    inputs = PROCESSOR(text = text, return_tensors="pt")
+
+    #Generate text embedding
+    with torch.no_grad():
+        text_embedding = MODEL.get_text_features(**inputs)
+
+    #Convert text embedding from a numpy array to a list
+    CACHED_TEXT_EMBEDDING = text_embedding.cpu().numpy().tolist()
+    end_time = time.time()
+    print(f"Get image embedding execution time: {(end_time - start_time) * 1000} ms")     
+    return CACHED_TEXT_EMBEDDING
+
+def pinecone_query(embedding): 
     start_time = time.time()
     pc = Pinecone(api_key=PINECONE_API_KEY)
     index = pc.Index(PINECONE_INDEX_NAME)
     images = []
     result = index.query(
-        vector=embedding,
+        vector=embedding, 
         top_k=10,
         include_metadata=True
     )
@@ -79,7 +108,7 @@ def pinecone_query(embedding):
 
 @app.get("/images")
 async def image_similarity_search():
-    image_embedding = get_embedding()
+    image_embedding = get_image_embedding()
     images, query_response_time = pinecone_query(image_embedding)
 
     return {"images": images, "query_response_time": query_response_time}
