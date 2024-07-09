486f6a718872cd5d95370e1e3a5d82d114e77927
TSCM-658 <thomas.m@pinecone.io>
2024-07-09 17:42:35 +0100

    Deleting unused files

--- image-search-at-scale/React/search-app/src/App.js
+++ image-search-at-scale/React/search-app/src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import './App.css'
 import api from './api'
 import ImageFetch from './ImageFetch';
-import DataTable from './DataTable';
 import PineconeLogo from './assets/pinecone-logo-black.png'
 import OriginalImage from './assets/image.jpeg'
 
