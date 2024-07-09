b5a28507ef3b0573c22f78bef0ad079632b83315
TSCM-658 <thomas.m@pinecone.io>
2024-07-09 17:38:38 +0100

    Refining UI

--- image-search-at-scale/React/search-app/src/ImageFetch.js
+++ image-search-at-scale/React/search-app/src/ImageFetch.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './ImageFetch.css';
+import './ImageFetch.css'; // Import the CSS file
 
 const ImageFetch = () => {
   const [images, setImages] = useState([]);
   const [error, setError] = useState(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/images')
@@ -24,32 +25,47 @@ const ImageFetch = () => {
       .catch(error => setError(error));
   }, []); // Empty dependency array means this effect runs once when the component mounts
 
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
   return (
-    <div className="image-grid-container">
-      <h1>Image Fetch</h1>
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div key={index} className="image-item">
-            <img
-              src={image.url} // Adjust based on your data structure
-              alt={`Image ${index}`}
-              className="image"
-            />
-            <div className="image-footer">
-              <p className="image-name">{image.caption}</p>
-              <p className="similarity-score">Similarity Score: {image.score}</p> {/* Adjust based on your data structure */}
+      <div className="image-grid-container">
+        <h1>Search Results</h1>
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="image-item"
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className="similarity-score">
+                Similarity Score: {image.score}
+              </div>
+              <img
+                src={image.url} // Adjust based on your data structure
+                alt={`Image ${index}`}
+                className="image"
+              />
+              <div className="image-footer">
+                <p className="image-title" style={{ opacity: hoveredIndex === index ? 1 : 0 }}>
+                  {image.caption}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default ImageFetch;
-
-
