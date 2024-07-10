48ece3047a95a175e7f142cad1d6572633c12df1
TSCM-658 <thomas.m@pinecone.io>
2024-07-10 13:15:22 +0100

    Text search WIP

--- image-search-at-scale/React/search-app/src/App.js
+++ image-search-at-scale/React/search-app/src/App.js
@@ -7,8 +7,11 @@ import OriginalImage from './assets/image.jpeg'
 import Dropzone from './components/Dropzone';
 
 function App() {
+
   const [searchMode, setSearchMode] = useState('text'); // State to toggle between text and image search
   const [files, setFiles] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleModeChange = (e) => {
     setSearchMode(e.target.value);
@@ -25,6 +28,30 @@ function App() {
       files.forEach((file) => URL.revokeObjectURL(file.preview))
     };
   }, [files]);
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleSearchSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/images', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ searchText }),
+      });
+      if (response.ok) {
+        const results = await response.json();
+        setSearchResults(results);
+      } else {
+        alert('Failed to fetch search results.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred.');  // add useEffect?????
+    }
+  };
 
   return (
     <div className="App">
@@ -58,8 +85,16 @@ function App() {
         <div className="search-container">
           {searchMode === 'text' ? (
             <div className="search-bar">
-              <input type="text" placeholder="Search..." className="search-input" />
-              <button className="search-button">Search</button>
+              <input
+                type="text"
+                placeholder="Search..."
+                className="search-input"
+                value={searchText}
+                onChange={handleSearchChange}
+              />
+              <button className="search-button" onClick={handleSearchSubmit}>
+                Search
+              </button>
             </div>
           ) : (
               <Dropzone onDrop={handleDrop} />
@@ -70,10 +105,11 @@ function App() {
           </div>
         </div>
       </header>
-      <ImageFetch />
+      <ImageFetch/>
     </div>
   );
 }
+
 
 
 
