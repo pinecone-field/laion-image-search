9b94a0717619589068fce3ccfd65d076d60605de
JamesS-Pinecone <james.s@pinecone.io>
2024-07-11 14:09:22 +0100

    Slider tabs now remember previous image/text searches

--- image-search-at-scale/React/search-app/src/App.js
+++ image-search-at-scale/React/search-app/src/App.js
@@ -1,106 +1,90 @@
-import React, {useState, useEffect} from 'react'
-import './App.css'
-import './components/fonts.css'
+import React, { useState } from 'react';
+import './App.css';
 import ImageFetch from './components/ImageFetch';
-import PineconeLogo from './assets/pinecone-logo-black.png'
-import OriginalImage from './assets/image.jpeg'
+import PineconeLogo from './assets/pinecone-logo-black.png';
+import OriginalImage from './assets/image.jpeg';
 import Dropzone from './components/Dropzone';
 import Search from './components/Search';
 import ImageDisplay from './components/ImageDisplay';
 import { ImageProvider } from './components/ImageContext';
 
 function App() {
-
-  const [searchMode, setSearchMode] = useState('text'); // State to toggle between text and image search
-  const [files, setFiles] = useState([]);
-  const [searchText, setSearchText] = useState('');
-
+  const [searchMode, setSearchMode] = useState('text');
+  const [textImages, setTextImages] = useState([]);
+  const [imageFiles, setImageFiles] = useState([]);
 
   const handleModeChange = (e) => {
     setSearchMode(e.target.value);
   };
 
   const handleDrop = (acceptedFiles) => {
-    setFiles(acceptedFiles.map((file) => Object.assign(file, {
+    setImageFiles(acceptedFiles.map((file) => Object.assign(file, {
       preview: URL.createObjectURL(file)
     })));
-  }
-
-  const handleSearchTextChange = (text) => {
-    setSearchText(text)
-  }
-
-  useEffect(() => {
-    return () => {
-      files.forEach((file) => URL.revokeObjectURL(file.preview))
-    };
-  }, [files]);
-  const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
   };
 
   return (
     <ImageProvider>
-    <div className="App">
-      <header className="App-header">
-        <div className="header-content">
-          <a href="https://www.pinecone.io" target="_blank">
-            <img src={PineconeLogo} alt="Pinecone Logo" className="pinecone-logo" />
+      <div className="App">
+        <header className="App-header">
+          <div className="header-content">
+            <a href="https://www.pinecone.io" target="_blank">
+              <img src={PineconeLogo} alt="Pinecone Logo" className="pinecone-logo" />
             </a>
-          <h1 className="header-title">Image Search Demo</h1>
-          <div className="slider-container">
-            <label className={`slider-label ${searchMode === 'text' ? 'active' : ''}`}>
-              <input
-                type="radio"
-                value="text"
-                checked={searchMode === 'text'}
-                onChange={handleModeChange}
-                className="slider-input"
-              />
-              Text
-            </label>
-            <label className={`slider-label ${searchMode === 'image' ? 'active' : ''}`}>
-              <input
-                type="radio"
-                value="image"
-                checked={searchMode === 'image'}
-                onChange={handleModeChange}
-                className="slider-input"
-              />
-              Image
-            </label>
-          </div>
-        </div>
-        <div className="search-container">
-          {searchMode === 'text' ? (
-            <div className="search-bar">
-              <Search searchMode={searchMode}/>
+            <h1 className="header-title">Image Search Demo</h1>
+            <div className="slider-container">
+              <label className={`slider-label ${searchMode === 'text' ? 'active' : ''}`}>
+                <input
+                  type="radio"
+                  value="text"
+                  checked={searchMode === 'text'}
+                  onChange={handleModeChange}
+                  className="slider-input"
+                />
+                Text
+              </label>
+              <label className={`slider-label ${searchMode === 'image' ? 'active' : ''}`}>
+                <input
+                  type="radio"
+                  value="image"
+                  checked={searchMode === 'image'}
+                  onChange={handleModeChange}
+                  className="slider-input"
+                />
+                Image
+              </label>
             </div>
-          ) : (
+          </div>
+          <div className="search-container">
+            {searchMode === 'text' ? (
+              <div className="search-bar">
+                <Search onSearchResults={setTextImages} />
+              </div>
+            ) : (
               <div className='image-search-wrapper'>
                 <div className='image-search-container'>
                   <Dropzone onDrop={handleDrop} />
-                    <div className="original-photo">
+                  <div className="original-photo">
                     <h2 className="original-photo-title">Photo to Search</h2>
                     <img src={OriginalImage} alt="Original Photo" className="original-photo-image" />
                   </div>
                 </div>
                 <div className='fetch-image-button'>
-                      <ImageFetch searchMode={searchMode}/>
-                    </div>
-              </div>  
-          )}
-        </div>
-      </header>
-      
-      <ImageDisplay set/>
-    </div>
+                  <ImageFetch onImagesFetched={setImageFiles} />
+                </div>
+              </div>
+            )}
+          </div>
+        </header>
+        
+        <ImageDisplay
+          searchMode={searchMode}
+          textImages={textImages}
+          imageFiles={imageFiles}
+        />
+      </div>
     </ImageProvider>
   );
 }
-
-
-
-
 
 export default App;
