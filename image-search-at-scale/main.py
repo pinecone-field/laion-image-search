1a43fde439779b0cabb30577bb803ba5da38d898
JamesS-Pinecone <james.s@pinecone.io>
2024-07-12 15:33:52 +0100

    Removes un-used files

--- image-search-at-scale/main.py
+++ image-search-at-scale/main.py
@@ -89,50 +89,6 @@ def get_image_embedding():
     print(f"Get image embedding execution time: {(end_time - start_time) * 1000} ms")
     return CACHED_EMBEDDING
 
-def get_text_embedding(text):
-    global CACHED_TEXT
-    global CACHED_TEXT_EMBEDDING
-    start_time = time.time()
-    #Commenting out as implementation has changed
-    #f = open(TEXT_PATH, 'w')
-    #text = f.read()
-    #If the text is the same then the embedding is the same
-    if text == CACHED_TEXT: 
-        print("Text has not changed. Using cached text embedding")
-        return CACHED_TEXT_EMBEDDING
-    CACHED_TEXT = text
-    inputs = PROCESSOR(text = text, return_tensors="pt")
-
-    #Generate text embedding
-    with torch.no_grad():
-        text_embedding = MODEL.get_text_features(**inputs)
-
-    #Convert text embedding from a numpy array to a list
-    CACHED_TEXT_EMBEDDING = text_embedding.cpu().numpy().tolist()
-    end_time = time.time()
-    print(f"Get image embedding execution time: {(end_time - start_time) * 1000} ms")     
-    return CACHED_TEXT_EMBEDDING
-
-def pinecone_query(embedding): 
-    start_time = time.time()
-    pc = Pinecone(api_key=PINECONE_API_KEY)
-    index = pc.Index(PINECONE_INDEX_NAME)
-    images = []
-    result = index.query(
-        vector=embedding, 
-        top_k=10,
-        include_metadata=True
-    )
-    
-    for m in result.matches:
-        images.append({
-                "caption": m.metadata["caption"],
-                "url": m.metadata["url"],
-                "score": m.score
-            })
-    query_response_time = round((time.time() - start_time) * 1000, 0)
-    print(f"Pinecone query execution time: {query_response_time} ms")
-
 def pinecone_query(embedding):
     
     pc = Pinecone(api_key=PINECONE_API_KEY)
@@ -180,11 +136,6 @@ def pinecone_query(embedding):
     f.close()
     return images, query_response_time
 
-def get_text_images(text):
-    text_embedding = get_text_embedding(text)
-    images, query_response_time = pinecone_query(text_embedding)
-    return [image for image in images]
-
 def get_url_status(url):
     try:
         code = requests.get(url, stream=True).status_code
@@ -222,10 +173,6 @@ async def upload_file(file:UploadFile = File(...)):
         return {"message": "Upload Successful!"}
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
-@app.post("/images")
-async def save_search(search_text: SearchText):
-    return get_text_images(search_text.searchText)
 
 
 app.mount("/", StaticFiles(directory="static"), name="static")
