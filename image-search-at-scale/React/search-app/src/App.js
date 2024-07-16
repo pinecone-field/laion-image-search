910328b895b8896d68d555a683895485c63db1ef
TSCM-658 <thomas.m@pinecone.io>
2024-07-16 17:59:26 +0100

    Removing slider from UI

--- image-search-at-scale/React/search-app/src/App.js
+++ image-search-at-scale/React/search-app/src/App.js
@@ -18,47 +18,18 @@ function App() {
         <div className="header-content">
           <img src={PineconeLogo} alt="Pinecone Logo" className="pinecone-logo" />
           <h1 className="header-title">Image Search Demo</h1>
-          <div className="slider-container">
-            <label className={`slider-label ${searchMode === 'text' ? 'active' : ''}`}>
-              <input
-                type="radio"
-                value="text"
-                checked={searchMode === 'text'}
-                onChange={handleModeChange}
-                className="slider-input"
-              />
-              Text
-            </label>
-            <label className={`slider-label ${searchMode === 'image' ? 'active' : ''}`}>
-              <input
-                type="radio"
-                value="image"
-                checked={searchMode === 'image'}
-                onChange={handleModeChange}
-                className="slider-input"
-              />
-              Image
-            </label>
-          </div>
         </div>
+      </header>
         <div className="search-container">
-          {searchMode === 'text' ? (
             <div className="search-bar">
               <input type="text" placeholder="Search..." className="search-input" />
               <button className="search-button">Search</button>
             </div>
-          ) : (
-            <div className="drag-drop-box">
-              <p>Drag and drop an image here</p>
-              {/* Implement drag and drop functionality here */}
-            </div>
-          )}
           <div className="original-photo">
             <h2 className="original-photo-title">Original Photo</h2>
             <img src={OriginalImage} alt="Original Photo" className="original-photo-image" />
           </div>
         </div>
-      </header>
       <ImageFetch />
     </div>
   );
